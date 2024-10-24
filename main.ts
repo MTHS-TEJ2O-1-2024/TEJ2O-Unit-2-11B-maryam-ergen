1ba5e0ac60712ffd464d2e475ec9db44222f3092
maryam-ergen <maryam.ergen@mths.ca>
2024-10-24 10:34:45 -0400

    made code

--- main.ts
+++ main.ts
@@ -1,8 +1,41 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Maryam Ergen
+ * Created on: Oct 2024
+ * This program compares numbers
 */
+let numberOne: number
+let numberTwo: number
 
-basic.showString('Hello, World!')
+numberOne = randint(0, 99)
+numberTwo = randint(0, 99)
+
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+input.onButtonPressed(Button.A, function () {
+    basic.showString("#1:" + (numberOne).toString())
+    basic.pause(1000)
+
+})
+
+input.onButtonPressed(Button.B, function () {
+    basic.showString("#2:" + (numberTwo).toString())
+    basic.pause(1000)
+
+})
+
+//whn microbit is shaken 
+input.onGesture(Gesture.Shake, function () {
+    if (numberOne < numberTwo) {
+        basic.clearScreen()
+        basic.showString((numberOne).toString() + "<" + (numberTwo).toString())
+        basic.pause(1000)
+    }
+    else {
+        basic.clearScreen()
+        basic.showString((numberOne).toString() + ">" + (numberTwo).toString())
+        basic.pause(1000)
+    }
+    basic.showIcon(IconNames.Sad)
+})
